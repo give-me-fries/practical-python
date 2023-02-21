04864816594770414c7868e85ed18c4c2a8efa43
fries <cmorrialt@gmail.com>
2023-02-21 06:57:25 -0800

    modified:   Work/pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,6 +3,7 @@
 # Exercise 1.27
 
 import csv
+import sys
 
 def portfolio_cost(filename):
     total = 0
@@ -16,5 +17,10 @@ def portfolio_cost(filename):
                 print("Cannot parse", row)
     return total
 
-cost = portfolio_cost('Data/portfolio.csv')
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
 print('Total cost:', cost)
