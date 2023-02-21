9794ecc959f53171474d68ea9190ea83d7c198c5
fries <cmorrialt@gmail.com>
2023-02-20 19:07:37 -0800

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total = 0
+with open("Data/portfolio.csv", "rt") as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(",")
+        total += int(row[1]) * float(row[2])
+
+print(f"Total cost {total}")
