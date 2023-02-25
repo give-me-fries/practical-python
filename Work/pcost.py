8a98eed08651f78a83a5ba5bd235fe1de60b8c2d
fries <cmorrialt@gmail.com>
2023-02-24 22:32:13 -0800

    modified:   Work/pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,16 +6,19 @@ import csv
 import sys
 
 def portfolio_cost(filename):
-    total = 0
+    total_cost = 0
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                total += int(row[1]) * float(row[2])
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
             except ValueError:
                 print(f'Row {rowno}: Bad row: {row}')
-    return total
+    return total_cost
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
