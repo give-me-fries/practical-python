24a06aaffe687ba66cf19855d4bee3826e4e6ab8
fries <cmorrialt@gmail.com>
2023-02-25 16:23:01 -0800

    modified:   Work/report.py

--- Work/report.py
+++ Work/report.py
@@ -43,13 +43,22 @@ def make_report(portfolio, prices):
     return rows
 
 
-portfolio = read_portfolio('Data/portfolio.csv')
-prices = read_prices('Data/prices.csv')
+def print_report(report):
+    res = ''
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print('%10s %10s %10s %10s' % headers)
+    print('---------- ---------- ---------- ----------')
+    for name, share, price, change in report:
+        print('%10s %10s %10s %10.2f' % (
+            name, share, '$' + str(price), change
+        ))
 
-report = make_report(portfolio, prices)
 
-headers = ('Name', 'Shares', 'Price', 'Change')
-print('%10s %10s %10s %10s' % headers)
-print('---------- ---------- ---------- ----------')
-for name, share, price, change in report:
-    print('%10s %10s %10s %10.2f' % (name, share, '$' + str(price), change))
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio, prices)
+    print_report(report)
+
+
+portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
