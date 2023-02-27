21993d18068b8c1f70dc9521ed7dc52a63c8d27c
fries <cmorrialt@gmail.com>
2023-02-26 18:42:23 -0800

    modified:   Work/fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -8,13 +8,17 @@ def parse_csv(
         select=None,
         types=None,
         has_headers=True,
-        delimiter=","
+        delimiter=",",
+        silence_errors = False
 ):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         
+        if select and not has_headers:
+            raise RuntimeError("select argument requires column headers")
+
         rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers
@@ -30,20 +34,27 @@ def parse_csv(
             indices = []
         
         records = []
-        for row in rows:
-            if not row:    # Skip rows with no data
-                continue
-            # Filter the row if specifc columns were selected
-            if indices:
-                row = [row[index] for index in indices]
-            if types:
-                row = [func(val) for func, val in zip(types, row)]
+        for rowNum, row in enumerate(rows, start=1):
+            try:
+                if not row:    # Skip rows with no data
+                    continue
+                # Filter the row if specifc columns were selected
+                if indices:
+                    row = [row[index] for index in indices]
+                if types:
+                        row = [func(val) for func, val in zip(types, row)]
+                # Make a dictionary
+                if has_headers:
+                    record = dict(zip(headers, row))
+                else:
+                    record = tuple(row)
+                records.append(record)
+            except ValueError as e:
+                if not silence_errors:
+                    print(
+                        f"Row {rowNum}: Couldn't convert {row}\n"
+                        f"Row {rowNum}: {e}"
+                    )
 
-            # Make a dictionary
-            if has_headers:
-                record = dict(zip(headers, row))
-            else:
-                record = tuple(row)
-            records.append(record)
 
     return records
